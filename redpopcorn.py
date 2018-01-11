96ffb98c7c3293c335b82a7946cf955154f8e14e
Joel <joelbenjamin093@gmail.com>
2018-01-12 00:03:42 +0530

    Make a nice UI

--- redpopcorn.py
+++ redpopcorn.py
@@ -3,7 +3,7 @@
 # main app
 
 from flask import Flask
-from flask import render_template,request
+from flask import render_template,request,redirect
 from indicer import MovieIndexer,load_movies
 
 app = Flask(__name__)
@@ -16,10 +16,20 @@ def get_movies(search):
     found_movies = list(corn.find_movie(heat_corn,search))
     return found_movies
 
-@app.route("/",methods=['GET'])
+@app.route("/")
 def index():
-    movie_list = get_movies("lol")
-    return render_template("home.html",movies=movie_list)
+    return render_template("home.html",movies=[])
+
+@app.route('/popcorns',methods=["POST"])
+def show_popcorns():
+    if request.method == "POST":
+        movie_name = request.form['search_box']
+        movie_list = get_movies(movie_name)
+        if movie_list:
+            return render_template("show_movies.html",movies=movie_list,search_text = movie_name)
+        return render_template("movie_not_found.html",error_text="Sorry could'nt find any movie")
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run(port=5000,debug=True)
